5b33f09bf35be1a98e9f6d9a4afebf1042b81e2d
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-02-19 16:57:48 +0530

    [Vinayak] connecting to the data base

---
+++ Employee_PayRoll__/Employee_PayRoll__/EmployeeRepo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_PayRoll__
+{
+    public class EmployeeRepo
+    {
+        //connecting string
+        public static string connectionString = @"Data Source=LAPTOP-DI3UPG04;Initial Catalog=Employee_payroll;Integrated Security=True";
+        SqlConnection connection = null;
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                //creating class object
+                EmployeePayRoll employeePayroll = new EmployeePayRoll();
+                using (connection = new SqlConnection(connectionString))
+                {
+                    string query = @"SELECT * FROM emp_payroll;";
+
+                    //define SqlCommand Object
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    connection.Open();
+                    Console.WriteLine("connected");
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            employeePayroll.employeeId = dr.GetInt32(0);
+                            employeePayroll.employeeName = dr.GetString(1);
+                            employeePayroll.basicPay = dr.GetDecimal(2);
+                            employeePayroll.startDate = dr.GetDateTime(3);
+                            employeePayroll.Gender = dr.GetString(4);
+                            employeePayroll.phoneNumber = dr.GetString(5);
+                            employeePayroll.address = dr.GetString(6);
+                            employeePayroll.department = dr.GetString(7);
+                            employeePayroll.deductions = dr.GetDecimal(8);
+                            employeePayroll.taxablePay = dr.GetDecimal(9);
+                            employeePayroll.tax = dr.GetDecimal(10);
+                            employeePayroll.netPay = dr.GetDecimal(11);
+
+                            //Display retrieved record
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", employeePayroll.employeeId, employeePayroll.employeeName, employeePayroll.phoneNumber, employeePayroll.address, employeePayroll.department, employeePayroll.Gender, employeePayroll.phoneNumber);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found!");
+                    }
+                    dr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                //close connection
+                connection.Close();
+            }
+        }
+    }  
+}
